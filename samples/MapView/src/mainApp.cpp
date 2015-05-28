05294197694a4aec66550419924990fe949a214b
marcelalcoverro <marcel.alcoverro@fezoo.cat>
2015-05-28 15:10:30 +0200

    adding mapView

---
+++ samples/MapView/src/mainApp.cpp
@@ -0,0 +1,172 @@
+#include "cinder/Cinder.h"
+#include "cinder/app/AppNative.h"
+#include "cinder/System.h"
+#include "cinder/Rand.h"
+#include "cinder/Thread.h"
+using namespace ci;
+using namespace ci::app;
+
+using namespace std;
+
+#include "Cinderactor.h"
+
+#include "mapTile.h"
+
+
+
+// We'll create a new Cinder Application by deriving from the BasicApp class
+class exampleApp : public AppNative {
+public:
+	void	prepareSettings( Settings *settings );
+    
+	void	mouseDown( MouseEvent event );
+	void	mouseDrag( MouseEvent event );
+    
+	void	touchesBegan( TouchEvent event );
+	void	touchesMoved( TouchEvent event );
+	void	touchesEnded( TouchEvent event );
+    
+	void	setup();
+    void    update();
+	void    draw();
+    void    shutdown();
+	void	keyDown( KeyEvent event ) { setFullScreen( ! isFullScreen() ); }
+    
+    void processThread();
+    
+    Cinderactor cinderactor;
+    
+    shared_ptr<std::thread>		mThread;
+    bool can_process_thread;
+    
+    bool init_ok;
+    
+    MapTile map_tile;
+
+    
+};
+
+void exampleApp::setup()
+{
+    
+    //Start interactor processing in a separate thread
+    can_process_thread = true;
+    mThread = shared_ptr<thread>( new thread( bind( &exampleApp::processThread, this ) ) );
+    
+    map_tile = MapTile(150);
+
+    setFullScreen(true);
+    
+    
+}
+
+void exampleApp::prepareSettings( Settings *settings )
+{
+	settings->enableMultiTouch();
+    settings->setWindowSize( 800, 600 );
+}
+
+void exampleApp::touchesBegan( TouchEvent event )
+{
+    //console() << "Began: " << event << std::endl;
+}
+
+void exampleApp::touchesMoved( TouchEvent event )
+{
+    //console() << "Moved: " << event << std::endl;
+}
+
+void exampleApp::touchesEnded( TouchEvent event )
+{
+    //console() << "Ended: " << event << std::endl;
+}
+
+void exampleApp::mouseDown( MouseEvent event )
+{
+    //	console() << "Mouse down: " << event.isRight() << " & " << event.isControlDown() << std::endl;
+}
+
+void exampleApp::mouseDrag( MouseEvent event )
+{
+    //	console() << "Mouse drag: " << std::endl;
+}
+
+void exampleApp::processThread()
+{
+   	ci::ThreadSetup threadSetup; // instantiate this if you're talking to Cinder from a secondary thread
+    
+    //Configure the cinderactor
+#ifdef _WIN32 // this flag is always defined in Windows 32/64
+	cinderactor.init("..\\resources\\interactor.cfg");
+#else
+    cinderactor.init( getResourcePath("interactor.cfg").string() );
+#endif
+    cinderactor.set_draw_window(false);
+    
+    // inifinite processing loop
+    while(can_process_thread)
+    {
+        cinderactor.process();
+    }
+}
+
+void exampleApp::update()
+{
+    if ( !map_tile.is_showing() )
+        map_tile.show();
+
+    
+//    // Detect hand strokes
+//    Cinderactor::StrokeType stroke = cinderactor.detect_hand_stroke( GEST_VICTORY );
+//    
+//    // Activate active_edge if stroke detected
+//    switch (stroke) {
+//        case Cinderactor::UP:
+//            if ( !map_tile.is_showing() )
+//                map_tile.show();
+//            break;
+//            
+//        case Cinderactor::DOWN:
+//            if ( map_tile.is_showing() )
+//                map_tile.hide();
+//            break;
+//            
+//        case Cinderactor::RIGHT:
+//            if ( !map_tile.is_showing() )
+//                map_tile.clear_canvas();
+//            break;
+//            
+//            
+//        default:
+//            break;
+//    }
+//    
+    map_tile.set_hands( cinderactor.get_hands() );
+    map_tile.update();
+
+    
+    
+    
+}
+
+void exampleApp::draw()
+{
+	gl::enableAlphaBlending();
+	gl::setMatricesWindow( getWindowSize() );
+	gl::clear( Color( 0.1f, 0.1f, 0.1f ) );
+    
+    
+    
+    map_tile.draw();
+
+    
+    cinderactor.draw();
+}
+
+void exampleApp::shutdown()
+{
+    can_process_thread = false;
+	mThread->join();
+}
+
+CINDER_APP_NATIVE( exampleApp, RendererGl )
