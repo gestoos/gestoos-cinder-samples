9edb739a71294f00738d9fcaf24a1fb8a3282d8a
xavisuau <xavi.suau@fezoo.cat>
2015-05-29 10:51:37 +0200

    adding missing files

---
+++ samples/ActiveEdges/include/resource.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "cinder/CinderResources.h"
+
+#define	W_CHART			CINDER_RESOURCE( ../resources/VALCRI_widgets/, w_chart.png, 128, PNG )
+#define	W_MAP			CINDER_RESOURCE( ../resources/VALCRI_widgets/, w_map.png, 129, PNG )
+#define	W_PERSONS       CINDER_RESOURCE( ../resources/VALCRI_widgets/, w_persons.png, 130, PNG )
+#define	W_ROUND			CINDER_RESOURCE( ../resources/VALCRI_widgets/, w_round.png, 131, PNG )
+
+#define	L_CHART			CINDER_RESOURCE( ../resources/VALCRI_widgets/, w_chart.png, 128, PNG )
+#define	L_MAP			CINDER_RESOURCE( ../resources/VALCRI_widgets/, w_map.png, 129, PNG )
+#define	L_PERSONS       CINDER_RESOURCE( ../resources/VALCRI_widgets/, w_persons.png, 130, PNG )
+#define	L_ROUND			CINDER_RESOURCE( ../resources/VALCRI_widgets/, w_round.png, 131, PNG )
+
+
+
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
