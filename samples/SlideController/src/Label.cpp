67208a9acbce336269fdd1eff471113eac7891ac
Adolfo López Méndez <adolfo.lopez.mendez@gmail.com>
2015-06-01 12:11:48 +0200

    Adding SlideController Sample

---
+++ samples/SlideController/src/Label.cpp
@@ -0,0 +1,103 @@
+//
+//  Label.cpp
+//  InTheCar
+//
+//  Created by Mac on 21/05/15.
+//
+//
+
+#include "Label.h"
+
+static int label_count = 0;
+
+Label::Label(){}
+
+Label::Label(const std::string & _t, float _w , Cinderactor::StrokeType _sense)
+{
+    
+    //inherited methods
+    text( _t );
+    size( _w, TextBox::GROW );
+    font( Font( "Arial", 60 ) );
+    alignment(  TextBox::CENTER  );
+    setColor( ColorA( 0.8f, 0.9f, 1.0f, alpha ) );
+    setBackgroundColor( ColorA( 0.3, 0.3, 0.3, alpha ) );
+    
+    //my vars
+    position = Vec2f(ci::app::getWindowWidth()/2.0, getWindowHeight()/2.0) - measure()/2.0 ; //centered
+    position.y += Rand::randInt(-30,30); //artistic random Y
+    sense = _sense;
+    dead = false;
+    alpha = 1.0;
+    
+    idnum = label_count++;
+    
+    X_TARGET_RIGHT = getWindowWidth();
+    X_TARGET_LEFT = 0;
+    Y_TARGET_UP = 0;
+    TARGET_ALPHA = 0.0;
+    
+    mTextTexture = gl::Texture( this->render() );
+    
+    std::cout<< idnum <<" +++ Label born "<<_t<<std::endl;
+    
+}
+
+Label::~Label()
+{
+    std::cout<< idnum <<" --- Label RIP "<<std::endl;
+}
+
+
+void Label::set_sense( Cinderactor::StrokeType _s )
+{
+    sense = _s;
+}
+
+
+void Label::signal_kill()
+{
+    dead = true;
+}
+
+bool Label::is_dead() const
+{
+    return dead;
+}
+
+void Label::update()
+{
+    //update pos
+    if( sense == Cinderactor::RIGHT )
+    {
+        position.x += ( X_TARGET_RIGHT - position.x ) * 0.08;
+    }
+    else if( sense == Cinderactor::LEFT )
+    {
+        position.x += ( X_TARGET_LEFT - position.x ) * 0.08;
+    }
+    else if( sense == Cinderactor::UP )
+    {
+        position.y += ( Y_TARGET_UP - position.y ) * 0.08;
+    }
+    
+    //Update alpha
+    alpha += (TARGET_ALPHA - alpha) * 0.08;
+    setBackgroundColor( ColorA( 1.0, 1.0, 1.0, alpha ) );
+    setColor( ColorA( 0.8f, 0.9f, 1.0f, alpha ) );
+    mTextTexture = gl::Texture( this->render() );
+    
+    
+    if( std::fabs( position.x - X_TARGET_RIGHT ) < 2 ||
+       std::fabs( position.x - X_TARGET_LEFT ) < 2 ||
+       std::fabs( position.y - Y_TARGET_UP ) < 2)
+    {
+        dead = true;
+    }
+}
+
+void Label::draw() const
+{
+    if( mTextTexture )
+		gl::draw( mTextTexture, position );
+}
---
+++ samples/SlideController/src/mainApp.cpp
@@ -0,0 +1,180 @@
+#include "cinder/Cinder.h"
+#include "cinder/app/AppNative.h"
+#include "cinder/System.h"
+#include "cinder/Rand.h"
+#include "cinder/Thread.h"
+//#include "cinder/Text.h"
+//#include "cinder/gl/gl.h"
+//#include "cinder/gl/Texture.h"
+using namespace ci;
+using namespace ci::app;
+
+#include <vector>
+#include <map>
+#include <list>
+using namespace std;
+
+//#include "fezoolib/NUI/Interactor.hpp"
+
+#include "Label.h"
+
+typedef boost::shared_ptr<Label> LabelPtr;
+
+float MIN_Z_VEL         = 30.0;
+
+// We'll create a new Cinder Application by deriving from the BasicApp class
+class exampleApp : public AppNative {
+public:
+	void	prepareSettings( Settings *settings );
+    
+	void	mouseDown( MouseEvent event );
+	void	mouseDrag( MouseEvent event );
+    
+	void	touchesBegan( TouchEvent event );
+	void	touchesMoved( TouchEvent event );
+	void	touchesEnded( TouchEvent event );
+    
+	void	setup();
+    void    update();
+	void    draw();
+    void    shutdown();
+	void	keyDown( KeyEvent event ) { setFullScreen( ! isFullScreen() ); }
+    
+    void processThread();
+
+    
+    
+    
+    //Cinderactor cinderactor;
+    Cinderactor cinderactor;
+
+    
+    shared_ptr<std::thread>		mThread;
+    bool can_process_thread;
+    bool init_ok;
+
+    std::list< LabelPtr > labels;
+};
+
+void exampleApp::setup()
+{
+    init_ok = false;
+    
+    //Start cinderactor processing in a separate thread
+    can_process_thread = true;
+    mThread = shared_ptr<thread>( new thread( bind( &exampleApp::processThread, this ) ) );
+ 
+    
+}
+
+void exampleApp::prepareSettings( Settings *settings )
+{
+	settings->enableMultiTouch();
+    settings->setWindowSize( 800, 600 );
+}
+
+void exampleApp::touchesBegan( TouchEvent event )
+{
+    //console() << "Began: " << event << std::endl;
+}
+
+void exampleApp::touchesMoved( TouchEvent event )
+{
+    //console() << "Moved: " << event << std::endl;
+}
+
+void exampleApp::touchesEnded( TouchEvent event )
+{
+    //console() << "Ended: " << event << std::endl;
+}
+
+void exampleApp::mouseDown( MouseEvent event )
+{
+    //	console() << "Mouse down: " << event.isRight() << " & " << event.isControlDown() << std::endl;
+}
+
+void exampleApp::mouseDrag( MouseEvent event )
+{
+    //	console() << "Mouse drag: " << std::endl;
+}
+
+void exampleApp::processThread()
+{
+   	ci::ThreadSetup threadSetup; // instantiate this if you're talking to Cinder from a secondary thread
+   
+    //Configure the cinderactor
+    cinderactor.init( getResourcePath("interactor.cfg").string() );
+    cinderactor.set_draw_window(false);
+    init_ok = true;
+    
+    // inifinite processing loop
+    while(can_process_thread)
+    {
+        cinderactor.process();
+    }
+}
+
+void exampleApp::update()
+{
+    // Update labels, and delete them if dead
+    for (auto it=labels.begin(); it!=labels.end(); ++it) {
+        (*it)->update();
+        if ((*it)->is_dead()) {
+            it = labels.erase(it);
+        }
+    }
+    
+    // Detect hand strokes
+    Cinderactor::StrokeType stroke = cinderactor.detect_hand_stroke( GEST_VICTORY );
+    
+    // Create labels if stroke detected
+    LabelPtr label_ptr;
+    switch (stroke) {
+        case Cinderactor::UP:
+            labels.push_back( LabelPtr( new Label("UP", 300, Cinderactor::UP) ) ) ;
+            break;
+            
+        case Cinderactor::RIGHT:
+            labels.push_back( LabelPtr( new Label("RIGHT", 300, Cinderactor::RIGHT) ) ) ;
+            break;
+            
+        case Cinderactor::LEFT:
+            labels.push_back( LabelPtr( new Label("LEFT", 300, Cinderactor::LEFT) ) ) ;
+            break;
+            
+        default:
+            break;
+    }
+}
+
+void exampleApp::draw()
+{
+	gl::enableAlphaBlending();
+	gl::setMatricesWindow( getWindowSize() );
+	gl::clear( Color( 0.1f, 0.13f, 0.16f ) );
+    
+    // Loading message
+    if( !init_ok )
+    {
+        gl::drawStringCentered(	"Loading models...please wait...", Vec2f( getWindowWidth()/2,getWindowHeight()/2 ) );
+        return;
+        
+    }
+
+    // Draw labels
+    for (auto it=labels.begin(); it!=labels.end(); ++it) {
+        (*it)->draw();
+    }
+    
+    // Draw cinderactor representation
+    cinderactor.draw();
+    
+}
+
+void exampleApp::shutdown()
+{
+    can_process_thread = false;
+	mThread->join();
+}
+
+CINDER_APP_NATIVE( exampleApp, RendererGl )
